95c1f93cdd8c06c8c50388d6703e15c23d8b4c03
hectorgz25@icloud.com <hectorgonzalez@Hectors-MacBook-Pro.local>
2022-08-24 15:44:56 -0600

    add colorize gem

---
+++ notes.rb
@@ -0,0 +1,19 @@
+#  Loop - loop through a code until condition is met
+#  iterators - go through each item of an array or hash.
+
+# num = 10
+
+# while num > 0
+#   puts num
+#   puts " hello"
+#   num -= 2
+# end
+
+# puts "1. to creat"
+# puts "2. to update"
+# puts "3. to exit"
+# user_iput = gets.strip.to_i
+
+# while user_input == 1 || user_input != 2 ||
+
+# end
